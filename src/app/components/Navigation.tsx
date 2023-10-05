ecca38940b5f3c0988ac7e36658deeb3bbb7e5fd
Lauri Mäkinen <lauri@mits.fi>
2023-10-05 14:20:52 +0300

    Use heroicons from React lib

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,28 +1,7 @@
 'use client';
 
 import React, { useState, MouseEventHandler } from 'react';
-
-type CloseIconProps = {
-  toggleMobileNavVisibility: MouseEventHandler<SVGSVGElement>;
-};
-
-const CloseIcon = ({ toggleMobileNavVisibility }: CloseIconProps) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="w-12 h-12 absolute top-3 right-3"
-    onClick={toggleMobileNavVisibility}
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
-);
+import { XCircleIcon } from '@heroicons/react/24/outline';
 
 const Navigation = () => {
   const [mobileNavOpen, setMobileNavOpen] = useState(true);
@@ -68,8 +47,9 @@ const Navigation = () => {
           <li className="p-5">Nav 3</li>
         </ul>
 
-        <CloseIcon
-          toggleMobileNavVisibility={() => setMobileNavOpen(!mobileNavOpen)}
+        <XCircleIcon
+          className="w-12 h-12 absolute top-2 right-2 text-slate-500"
+          onClick={() => setMobileNavOpen(!mobileNavOpen)}
         />
       </div>
     </nav>
