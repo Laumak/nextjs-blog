d07fe1caf1f3cf8bfa2dfbd63dfa99977c99e574
Lauri Mäkinen <lauri@mits.fi>
2023-10-05 22:18:13 +0300

    Move nav to global layout and add test news

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import Navigation from './components/Navigation';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -16,7 +18,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Navigation />
+        </header>
+
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,62 @@
 import React from 'react';
 
-import Navigation from './components/Navigation';
-
 export default function Home() {
   return (
-    <React.Fragment>
-      <header>
-        <Navigation />
-      </header>
+    <main className="mt-28">
+      <article className="mb-10">
+        <h2 className="text-2xl">Quis duis qui nisi in cupidatat</h2>
+        <small>10.10.2023</small>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+      </article>
 
-      <main>
-        <div className="flex items-center justify-center h-screen">
-          <h1 className="text-6xl">Hello world!</h1>
-        </div>
-      </main>
-    </React.Fragment>
+      <article className="mb-10">
+        <h2 className="text-2xl">Quis duis qui nisi in cupidatat</h2>
+        <small>10.10.2023</small>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+      </article>
+
+      <article className="mb-10">
+        <h2 className="text-2xl">Quis duis qui nisi in cupidatat</h2>
+        <small>10.10.2023</small>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+      </article>
+
+      <article className="mb-10">
+        <h2 className="text-2xl">Quis duis qui nisi in cupidatat</h2>
+        <small>10.10.2023</small>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+      </article>
+
+      <article className="mb-10">
+        <h2 className="text-2xl">Quis duis qui nisi in cupidatat</h2>
+        <small>10.10.2023</small>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+      </article>
+    </main>
   );
 }
