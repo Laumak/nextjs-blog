cd71e84094cfff728119df75b6281bb7eb19a2e3
Lauri Mäkinen <lauri@mits.fi>
2023-10-05 22:18:37 +0300

    Create initial individual blog page

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function BlogPage() {
+  return (
+    <main className="mt-28 mb-5">
+      <article>
+        <h1 className="text-2xl">Quis duis qui nisi in cupidatat</h1>
+
+        <small>10.10.2023</small>
+
+        <Image className="h-auto max-w-full" fill sizes="100%" src="/test.jpg" alt="image description"></Image>
+
+        <p className="mb-5">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+          blanditiis consequatur voluptatem. Qui magnam ratione dicta
+          accusantium perferendis recusandae quo in itaque, nam eligendi
+          repudiandae aut ea voluptatum, tempora magni.
+        </p>
+
+        <p className="mb-5">
+          Ut enim ad voluptate eu magna excepteur eiusmod. Incididunt irure enim adipisicing exercitation in. Eu adipisicing enim in non nisi. Labore mollit deserunt consequat ut nulla enim velit.
+        </p>
+        
+        <p className="mb-5">
+          Excepteur dolor ullamco duis in in exercitation enim minim. Dolor sint ad quis Lorem amet est. Elit deserunt non aliqua id ullamco ad occaecat ullamco labore enim. Anim sunt ut id eu ut adipisicing dolore aute proident culpa sit aliquip consequat laboris.
+        </p>
+
+        <p className="mb-5">
+          Quis ipsum consequat laborum consequat est magna id. Proident commodo aliqua sint eu tempor proident officia proident aliqua in commodo eiusmod qui quis. Eu anim eu ipsum anim duis esse. Velit amet deserunt laboris consequat excepteur amet pariatur irure. Sint sit Lorem consectetur labore dolor culpa. Laborum laborum elit enim nostrud duis ea. Id proident consectetur tempor eu qui sit commodo duis sunt.
+        </p>
+
+        <p className="mb-5">
+          Quis ipsum consequat laborum consequat est magna id. Proident commodo aliqua sint eu tempor proident officia proident aliqua in commodo eiusmod qui quis. Eu anim eu ipsum anim duis esse. Velit amet deserunt laboris consequat excepteur amet pariatur irure. Sint sit Lorem consectetur labore dolor culpa. Laborum laborum elit enim nostrud duis ea. Id proident consectetur tempor eu qui sit commodo duis sunt.
+        </p>
+
+        <p className="mb-5">
+          Quis ipsum consequat laborum consequat est magna id. Proident commodo aliqua sint eu tempor proident officia proident aliqua in commodo eiusmod qui quis. Eu anim eu ipsum anim duis esse. Velit amet deserunt laboris consequat excepteur amet pariatur irure. Sint sit Lorem consectetur labore dolor culpa. Laborum laborum elit enim nostrud duis ea. Id proident consectetur tempor eu qui sit commodo duis sunt.
+        </p>
+
+        <p>
+          Quis ipsum consequat laborum consequat est magna id. Proident commodo aliqua sint eu tempor proident officia proident aliqua in commodo eiusmod qui quis. Eu anim eu ipsum anim duis esse. Velit amet deserunt laboris consequat excepteur amet pariatur irure. Sint sit Lorem consectetur labore dolor culpa. Laborum laborum elit enim nostrud duis ea. Id proident consectetur tempor eu qui sit commodo duis sunt.
+        </p>
+      </article>
+    </main>
+  );
+}
