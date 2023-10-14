e94ba5fe49690a091068a69bb8cb737d3a12e0b2
Lauri Mäkinen <lauri@mits.fi>
2023-10-14 14:00:28 +0300

    Tag component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
+interface TagProps {
+  children: string;
+  backgroundHue: 400 | 600 | 800;
+}
+
+const Tag = ({ children, backgroundHue }: TagProps) => {
+  let classes = `bg-slate-${backgroundHue} rounded-md py-1 px-2 mr-1 inline-block`;
+
+  // Make text color white if background goes "dark enough"
+  // to not be accessible anymore.
+  if (backgroundHue >= 600) {
+    classes += ' text-white';
+  }
+
+  return (
+    <li className={classes}>
+      <Link href="/tags/wip">{children}</Link>
+    </li>
+  );
+};
+
 export default function Home() {
   return (
     <React.Fragment>
@@ -8,14 +29,12 @@ export default function Home() {
         <h2 className="text-2xl">
           <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
         </h2>
-        <small>
-          10.10.2023 |{' '}
-          <code className="bg-slate-400 rounded-md p-1">test 1</code>
-          &nbsp;
-          <code className="bg-slate-600 text-white rounded-md p-1">test 2</code>
-          &nbsp;
-          <code className="bg-slate-800 text-white rounded-md p-1">test 3</code>
-        </small>
+        <div className="my-2 text-slate-600">Published on 10.10.2023</div>
+        <ul aria-label="Tags" className="my-2">
+          <Tag backgroundHue={400}>Test 1</Tag>
+          <Tag backgroundHue={600}>Test 2</Tag>
+          <Tag backgroundHue={800}>Test 3</Tag>
+        </ul>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
           blanditiis consequatur voluptatem. Qui magnam ratione dicta
