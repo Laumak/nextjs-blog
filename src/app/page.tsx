60304e695c3a25a54704de889989ae01cf688cb8
Lauri Mäkinen <lauri@mits.fi>
2023-10-14 13:23:20 +0300

    Test out tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,14 @@ export default function Home() {
         <h2 className="text-2xl">
           <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
         </h2>
-        <small>10.10.2023</small>
+        <small>
+          10.10.2023 |{' '}
+          <code className="bg-slate-400 rounded-md p-1">test 1</code>
+          &nbsp;
+          <code className="bg-slate-600 text-white rounded-md p-1">test 2</code>
+          &nbsp;
+          <code className="bg-slate-800 text-white rounded-md p-1">test 3</code>
+        </small>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
           blanditiis consequatur voluptatem. Qui magnam ratione dicta
