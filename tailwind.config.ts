71dcccd3d52f350ef15546fe4aca2154a0403ca6
Lauri Mäkinen <lauri@mits.fi>
2023-10-14 22:28:50 +0300

    "safeList" dynamic Tag component bg color CSS classes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,15 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  // https://tailwindcss.com/docs/content-configuration#safelisting-classes
+  safelist: [
+    // As these classes are used dynamically in the "Tag" component,
+    // they're not present in the end-result before runtime. Due to this
+    // the classes have to manually be "forced" to be in the css output.
+    'bg-slate-400',
+    'bg-slate-600',
+    'bg-slate-800',
+  ],
   theme: {
     extend: {
       backgroundImage: {
