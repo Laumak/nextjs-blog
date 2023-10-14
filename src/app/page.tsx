a7aae13b7097a9ee10953c35f4c7d199c2b2fe68
Lauri Mäkinen <lauri@mits.fi>
2023-10-14 14:04:00 +0300

    New barebones (frontpage) Article component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,78 +22,59 @@ const Tag = ({ children, backgroundHue }: TagProps) => {
   );
 };
 
+interface ArticleProps {
+  children: string;
+  title: string;
+  link: string;
+}
+
+const Article = ({ children, title, link }: ArticleProps) => {
+  return (
+    <article className="mt-6 mb-10">
+      <h2 className="text-2xl">
+        <Link href={link}>{title}</Link>
+      </h2>
+      <div className="my-2 text-slate-600">Published on 10.10.2023</div>
+      <ul aria-label="Tags" className="my-2">
+        <Tag backgroundHue={400}>Test 1</Tag>
+        <Tag backgroundHue={600}>Test 2</Tag>
+        <Tag backgroundHue={800}>Test 3</Tag>
+      </ul>
+      <p>{children}</p>
+    </article>
+  );
+};
+
 export default function Home() {
   return (
     <React.Fragment>
-      <article className="mt-6 mb-10">
-        <h2 className="text-2xl">
-          <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
-        </h2>
-        <div className="my-2 text-slate-600">Published on 10.10.2023</div>
-        <ul aria-label="Tags" className="my-2">
-          <Tag backgroundHue={400}>Test 1</Tag>
-          <Tag backgroundHue={600}>Test 2</Tag>
-          <Tag backgroundHue={800}>Test 3</Tag>
-        </ul>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          blanditiis consequatur voluptatem. Qui magnam ratione dicta
-          accusantium perferendis recusandae quo in itaque, nam eligendi
-          repudiandae aut ea voluptatum, tempora magni.
-        </p>
-      </article>
+      <Article title="Quis duis qui nisi in cupidatat" link="/blog/test">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        blanditiis consequatur voluptatem. Qui magnam ratione dicta accusantium
+        perferendis recusandae quo in itaque, nam eligendi repudiandae aut ea
+        voluptatum, tempora magni.
+      </Article>
 
-      <article className="mb-10">
-        <h2 className="text-2xl">
-          <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
-        </h2>
-        <small>10.10.2023</small>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          blanditiis consequatur voluptatem. Qui magnam ratione dicta
-          accusantium perferendis recusandae quo in itaque, nam eligendi
-          repudiandae aut ea voluptatum, tempora magni.
-        </p>
-      </article>
+      <Article title="Quis duis qui nisi in cupidatat" link="/blog/test">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        blanditiis consequatur voluptatem. Qui magnam ratione dicta accusantium
+        perferendis recusandae quo in itaque, nam eligendi repudiandae aut ea
+        voluptatum, tempora magni.
+      </Article>
 
-      <article className="mb-10">
-        <h2 className="text-2xl">
-          <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
-        </h2>
-        <small>10.10.2023</small>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          blanditiis consequatur voluptatem. Qui magnam ratione dicta
-          accusantium perferendis recusandae quo in itaque, nam eligendi
-          repudiandae aut ea voluptatum, tempora magni.
-        </p>
-      </article>
+      <Article title="Quis duis qui nisi in cupidatat" link="/blog/test">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        blanditiis consequatur voluptatem. Qui magnam ratione dicta accusantium
+        perferendis recusandae quo in itaque, nam eligendi repudiandae aut ea
+        voluptatum, tempora magni.
+      </Article>
 
-      <article className="mb-10">
-        <h2 className="text-2xl">
-          <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
-        </h2>
-        <small>10.10.2023</small>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          blanditiis consequatur voluptatem. Qui magnam ratione dicta
-          accusantium perferendis recusandae quo in itaque, nam eligendi
-          repudiandae aut ea voluptatum, tempora magni.
-        </p>
-      </article>
-
-      <article className="mb-10">
-        <h2 className="text-2xl">
-          <Link href="/blog/test">Quis duis qui nisi in cupidatat</Link>
-        </h2>
-        <small>10.10.2023</small>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
-          blanditiis consequatur voluptatem. Qui magnam ratione dicta
-          accusantium perferendis recusandae quo in itaque, nam eligendi
-          repudiandae aut ea voluptatum, tempora magni.
-        </p>
-      </article>
+      <Article title="Quis duis qui nisi in cupidatat" link="/blog/test">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat
+        blanditiis consequatur voluptatem. Qui magnam ratione dicta accusantium
+        perferendis recusandae quo in itaque, nam eligendi repudiandae aut ea
+        voluptatum, tempora magni.
+      </Article>
     </React.Fragment>
   );
 }
